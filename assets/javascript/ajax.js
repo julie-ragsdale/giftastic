db8e5bd339de201b982d79c17a1c18f3f09619b0
leaf-junkie <ragsdale.jar@gmail.com>
2019-01-18 15:09:37 -0500

    Adopted BEM naming convention
    Renamed Ids and classes

--- assets/javascript/ajax.js
+++ assets/javascript/ajax.js
@@ -17,7 +17,13 @@ $(document).ready(function() {
         'houseplant',
         'coding',
         'science',
-        'botany'
+        'botany',
+        'overcast',
+        'cozy',
+        'dog',
+        'cat',
+        'embroidery',
+        'sewing'
     ]
     // have function that adds custom search term to array and then recall that function to fill in new button
     function renderButtons() {
@@ -61,26 +67,37 @@ $(document).ready(function() {
         });
     });
         
-        // Your app should take the topics in this array and create buttons in your HTML.
-        var topics = response.Search;
-        
-        // Try using a loop that appends a button for each string in the array.
-        for (i = 0; i < topics.length; i ++) {
-            var buttonDiv = $('#buttons');
-            var button = $('<button>').text(response.Search); // Not sure if this should be "Search"
-        }
-        
-        button.append(response.Search);
-        buttonDiv.append(btn);
-        
-        // When the user clicks on a button, the page should grab 10 static, non-animated gif images from the GIPHY API and place them on the page.
-        
-        // When the user clicks one of the still GIPHY images, the gif should animate. If the user clicks the gif again, it should stop playing.
-        
-        // Under every gif, display its rating (PG, G, so on).
-        // This data is provided by the GIPHY API.
-        // Only once you get images displaying with button presses should you move on to the next step.
-        
+    renderButtons();
+
+    var topics = response.Search;
+    var rating = response.rating;
+    var
+    
+    // Your app should take the topics in your array and create buttons in your HTML
+    
+    // Use a for loop that appends a button for each string in the array
+    for (i = 0; i < topics.length; i ++) {
+        var buttonDiv = $('#buttons');
+        var button = $('<button>').text(response.Search); // Not sure if this should be "Search"
+    }
+    
+    button.append(response.Search);
+    buttonDiv.append(btn);
+    
+    // When the user clicks on a button, the page should grab 10 static, non-animated gif images from the GIPHY API and place them on the page.
+    
+
+    // When the user clicks one of the still GIPHY images, the gif should animate. If the user clicks the gif again, it should stop playing.
+    
+
+    // Under every gif, display its rating (PG, G, so on).
+    
+
+    // This data is provided by the GIPHY API.
+    
+    
+    // Only once you get images displaying with button presses should you move on to the next step.
+    
     
     // Add a form to your page takes the value from a user input box and adds it into your topics array. Then make a function call that takes each topic in the array remakes the buttons on the page.
     
