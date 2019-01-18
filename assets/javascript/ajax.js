a1d45904e1379b7b78d262d530603bc2c85b2e13
leaf-junkie <ragsdale.jar@gmail.com>
2019-01-18 14:52:02 -0500

    JavaScript
    Added API key
    AJAX configuration
    Added function to render new buttons on the fly

--- assets/javascript/ajax.js
+++ assets/javascript/ajax.js
@@ -1,11 +1,9 @@
-// GIPHY API key: KhYgqTlp6zKKgwSZRM66bvMXcRJIm070
-
 // q - search query term or phrase 
 // limit - (optional) number of results to return, maximum 100. Default 25.
 // rating - limit results to those rated (y,g, pg, pg-13 or r).
 
-$('document').ready(function() {
-
+$(document).ready(function() {
+    console.log( "ready!" );
     // Array of search terms
     var topics = [
         'puppy',
@@ -17,46 +15,76 @@ $('document').ready(function() {
         'art',
         'plants',
         'houseplant',
-        'coding'
+        'coding',
+        'science',
+        'botany'
     ]
+    // have function that adds custom search term to array and then recall that function to fill in new button
+    function renderButtons() {
 
-    // Require with custom API key
-    var giphy = require('giphy-api')('KhYgqTlp6zKKgwSZRM66bvMXcRJIm070');
 
-    var queryURL = $.get("https://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=YOUR_API_KEY&limit=10");
+        $('#button-grid').empty();
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function(response) {
-        console.log(response)
-    ;
+        topics.forEach(function(topic) {
 
-    // Your app should take the topics in this array and create buttons in your HTML.
-    var topics = response.Search;
-
-    // Try using a loop that appends a button for each string in the array.
-    for (i = 0; i < topics.length; i ++) {
-        var buttonDiv = $('#buttons');
-        var btn = $('<button>').text(response.Search); // Not sure if this should be "Search"
+            var button = $('<button>');
+                button.addClass('topic');
+                button.attr('data-name', topic);
+                button.text(topic);
+                $('')
+        }); 
     }
 
-    btn.append(response.Search);
-    buttonDiv.append(btn);
-    
-    // When the user clicks on a button, the page should grab 10 static, non-animated gif images from the GIPHY API and place them on the page.
-
-    // When the user clicks one of the still GIPHY images, the gif should animate. If the user clicks the gif again, it should stop playing.
-
-    // Under every gif, display its rating (PG, G, so on).
-        // This data is provided by the GIPHY API.
-        // Only once you get images displaying with button presses should you move on to the next step.
-
+    // Event handler for button click
+    $('#...').on('click', function(event) {
+        event.preventDefault;
+        var value = $('#...').val().trim();
+        topics.push(value);
+        renderButtons();
     });
 
+    $(document).on('click', '. ...', function() {
+        
+        // Refers to the button being clicked
+        var topicNew = $(this).attr('data-name');
+        
+        // Giphy API key
+        var giphyAPI = 'KhYgqTlp6zKKgwSZRM66bvMXcRJIm070';
+        var queryURL = 'https://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=KhYgqTlp6zKKgwSZRM66bvMXcRJIm070&limit=10';
+        
+        // AJAX configuration
+        $.ajax({
+            url: queryURL,
+            method: 'GET'
+        }).then(function(response) { // Use 'then' as a promise (allows you to chain multiple calls; one doesnt execute before the previous one is finished; prevent callback hell)
+            console.log(response);
+        });
+    });
+        
+        // Your app should take the topics in this array and create buttons in your HTML.
+        var topics = response.Search;
+        
+        // Try using a loop that appends a button for each string in the array.
+        for (i = 0; i < topics.length; i ++) {
+            var buttonDiv = $('#buttons');
+            var button = $('<button>').text(response.Search); // Not sure if this should be "Search"
+        }
+        
+        button.append(response.Search);
+        buttonDiv.append(btn);
+        
+        // When the user clicks on a button, the page should grab 10 static, non-animated gif images from the GIPHY API and place them on the page.
+        
+        // When the user clicks one of the still GIPHY images, the gif should animate. If the user clicks the gif again, it should stop playing.
+        
+        // Under every gif, display its rating (PG, G, so on).
+        // This data is provided by the GIPHY API.
+        // Only once you get images displaying with button presses should you move on to the next step.
+        
+    
     // Add a form to your page takes the value from a user input box and adds it into your topics array. Then make a function call that takes each topic in the array remakes the buttons on the page.
-
-)};
+    
+});
 
 // Deploy your assignment to Github Pages
 
