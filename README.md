7eb9ac2edef78f66e479aa9735574fd664f0a987
leaf-junkie <ragsdale.jar@gmail.com>
2020-09-20 23:41:36 -0400

    Update style

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 Link to site: https://leaf-junkie.github.io/giftastic/
 
-This assignment focuses on using AJAX to call Giphy's API. Buttons and images are rendered on the fly, so the HTML file is sparse.  
+This is a fun little web app that uses AJAX to query the Giphy API. The user can select from one of the existing topics or enter their own to search. The layout is generated with CSS grid.
+
+Buttons and images are rendered on the fly, so the HTML file is sparse.  
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,60 +2,49 @@
 
 $(document).ready(function() {
 
-    var topics = [
-        'red panda',
-        'dog',
-        'cat',
-        'bearded dragon',
-        'husky',
-        'guinea pig',
-        'parakeet',
-        'hamster',
-        'bunny',
-        'fox',
-        'raccoon'
+    let topics = [
+        'cats',
+        'dogs',
+        'the office',
+        'parks and rec',
+        'houseplants',
+        'coffee',
+        'vaporwave',
+        'coding',
+        'ghibli',
+        'hacker',
+        'pokemon',
+        'fallout'
     ];
     
     $(document).on('click', '.topic', function() {
-        var topic = $(this).attr('data-id');
-        var queryURL = 'https://api.giphy.com/v1/gifs/search?api_key=KhYgqTlp6zKKgwSZRM66bvMXcRJIm070&q=' + topic + '&limit=10&lang=en';
+        // empty main grid before appending anything else to it
+        $('.grid').empty();
+        $('#selectTheme').css({display: 'none'});
+
+        const topic = $(this).attr('data-id');
+        const queryURL = 'https://api.giphy.com/v1/gifs/search?api_key=KhYgqTlp6zKKgwSZRM66bvMXcRJIm070&q=' + topic + '&limit=12&lang=en';
 
         $.ajax({
             url: queryURL,
             method: 'GET'
-        }).then(function(response) {
-            $('<button>').text(response.search);
-            var button = $('<button>').text(response.Search);
-            button.append(response.Search);
+        }).then(res=> {
+            $('<button>').text(res.search);
+            var button = $('<button>').text(res.Search);
+            button.append(res.Search);
             $('.gallery').empty();
 
-        // Ignore - I plan on using a modulo to create a grid that will place items from the array into the grid based on their index
-        //  Will use modulo because the grid will have multiple rows, so finding the remainder will serve as an identifier in a two dimensional array
-            // This is my gallery grid
-            // $('').append(gifDiv); // Row 1 Col 1
-            // $('').append(gifDiv); // Row 1 Col 2
-            // $('').append(gifDiv); // Row 1 Col 3
-            // $('').append(gifDiv); // Row 1 Col 4
-            // $('').append(gifDiv); // Row 1 Col 5
-
-            // $('').append(gifDiv); // Row 2 Col 1
-            // $('').append(gifDiv); // Row 2 Col 2
-            // $('').append(gifDiv); // Row 2 Col 3
-            // $('').append(gifDiv); // Row 2 Col 4
-            // $('').append(gifDiv); // Row 2 Col 5
-
-            // Loop through all topics in array
-            for (i = 0; i < response.data.length; i ++) {
-                var imageUrl = response.data[i].images.original_still.url;
-                var stillUrl = response.data[i].images.original_still.url;
-                var animateUrl = response.data[i].images.original.url;
-                var gif = $('<img>');
-                var gifTitle = $('<p>').text('Title: ' + response.data[i].title);
-                var gifRating = $('<p>').text('Rated: ' + response.data[i].rating);
-                var gifDiv = $('<div class="gallery-item clearfix">');
+            // Append desired data from response
+            for (i = 0; i < res.data.length; i ++) {
+                const imageUrl = res.data[i].images.original_still.url;
+                const stillUrl = res.data[i].images.original_still.url;
+                const animateUrl = res.data[i].images.original.url;
+                const gif = $('<img>');
+                const gifTitle = $('<p>').text(res.data[i].title);
+                const gifRating = $('<p>').text(`Rated: ${res.data[i].rating}`);
+                const gifDiv = $('<div class="gallery-item">');
                 
-                // Looks as if this line is grabbing the correct URL, but I am getting an error about missing a closing parenthesis
-                var dlBtn = $('<button type="submit" onclick="window.open(' + imageUrl + ')">').text('Download!');
+                // const dlBtn = $('<button type="submit" onclick="window.open(' + imageUrl + ')">').text('Download!');
                 
                 $('.gallery-header').css({visibility: 'visible'});
                 gif.attr('src', imageUrl);
@@ -65,56 +54,47 @@ $(document).ready(function() {
                 gif.css({
                     border: '2px solid #fff',
                     borderRadius: '25px',
-                    maxWidth: '200px',
-                    float: 'left'
+                    width: '100%',
+                    float: 'left',
+                    cursor: 'pointer'
                 });
                 gifTitle.css({
-                    color: '#333',
-                    fontSize: '24px'
-                });
-                dlBtn.css({
-                    backgroundColor: '#fc766a',
                     color: '#fff',
-                    fontFamily: '',
-                    fontSize: '20px',
-                    border: '2px solid #fff',
-                    borderRadius: '25px',
+                    fontSize: '20px'
                 });
+                // dlBtn.css({
+                //     backgroundColor: '#111',
+                //     color: '#fff',
+                //     fontFamily: '',
+                //     fontSize: '20px',
+                //     border: '2px solid #fff',
+                //     borderRadius: '5px',
+                //     cursor: 'pointer'
+                // });
                 gifDiv.append(gif);
                 gifDiv.append(gifTitle);
                 gifDiv.append(gifRating);
-                gifDiv.append(dlBtn);
+                // gifDiv.append(dlBtn);
                 $('.gallery-item').css({
-                    maxWidth: '200px',
-                    float: 'left',
-                    margin: '25px',
+                    marginRight: '30px',
+                    textTransform: 'lowercase'
                 });
 
-                // Append items to respective rows
-                if (i <= 4) {
-                    $('#gallery_row-1').append(gifDiv);
-                
-                // The next four gifs (5 through 8) should be appended to the second row 
-                } else if ( 4 < i < 9) {
-                    $('#gallery_row-2').append(gifDiv);
-
-                // The last two gifs should be appended to the third row - Unsure of why they are not
-                } else {
-                    $('#gallery_row-3').append(gifDiv);
-                }
+                // Append items
+                $('.grid').append(gifDiv);
             }
         });
     });   
     
     // On click, have the gif animate or be still
-    $('.gallery').on('click', 'img', function(){
-        var state = $(this).attr('data-state');
+    $('.gallery').on('click', 'img', ()=> {
+        const state = $(this).attr('data-state');
         if (state === 'still') {
-            var url = $(this).attr('data-animate');
+            let url = $(this).attr('data-animate');
             $(this).attr('src', url);
             $(this).attr('data-state', 'animate');
         } else {
-            var url = $(this).attr('data-still');
+            let url = $(this).attr('data-still');
             $(this).attr('src', url);
             $(this).attr('data-state', 'still');
         }
@@ -124,33 +104,36 @@ $(document).ready(function() {
     function renderButtons() {
         $('#button-grid').empty();
         topics.forEach(function(topic) {
-            var button = $('<button>');
+            const button = $('<button>');
             button.addClass('topic');
             button.attr('data-id', topic);
             button.text(topic);
-            button.css({margin: '15px',
+            button.css({
+                margin: '4px',
+                padding: '12px',
                 position: 'relative',
-                width: '200px',
-                height: '50px',
+                textAlign: 'left',
+                width: '80%',
                 fontSize: '20px',
                 fontFamily: "'Segoe UI', Tahoma, Geneva, Verdana, sansSerif",
                 backgroundColor: '#333',
                 color: '#fff',
-                border: '2px solid #fff',
-                borderRadius: '25px'
+                border: '1px solid #fff',
+                borderRadius: '5px',
+                cursor: 'pointer'
             });
             $('#button-grid').append(button);
         }); 
     }
     renderButtons();
         
-    // On click, push user input into the topics array
-    $('#searchbar_add-button').on('click', function(event) {
+    // On click, push user input into topics array
+    $('#searchbar_add-button').on('click', event=> {
         event.preventDefault();
-        var value = $('#searchbar-input').val().trim();
+        const value = $('#searchbar').val().trim();
         topics.push(value);
         renderButtons();
-        $('#searchbar-input').val('');
+        $('#searchbar').val('');
         $('.gallery').empty();
     });
 });
